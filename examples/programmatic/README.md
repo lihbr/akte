3b6d2ccb22a93a350f38c6bbf83e280c46f048c7
lihbr <lihbr@users.noreply.github.com>
2023-01-09 22:43:58 +0100

    docs: add programmatic example

---
+++ examples/programmatic/README.md
@@ -0,0 +1,5 @@
+# Programmatic
+
+This example shows programmatic usage of Akte. This is helpful for running Akte in various environments, including serverless.
+
+Peek inside `programmatic.ts` to see some example usage of Akte API.
---
+++ examples/programmatic/programmatic.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { app } from "./akte.app";
+
+// Renders all files and returns them.
+const files = await app.renderAll();
+
+// Renders all files and returns them.
+await app.writeAll({ files, outDir: "my-out-dir" });
+
+// Renders and writes all files to the config output directory.
+await app.buildAll();
+
+// Looks up the Akte file responsible for rendering the given path.
+const match = app.lookup("/foo");
+
+// Renders a match from `app.lookup()`
+const file = await app.render(match);
